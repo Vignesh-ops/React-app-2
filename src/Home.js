c7b25220cc8643e02398058188be879c81f5cfcd
Vignesh-ops <vignesh282282@gmail.com>
2025-01-18 04:00:20 -0500

    Added Loading skeleten

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,52 @@
 import React, { useContext } from 'react'
 import Feed from './Feed'
 import DataContext from './context/DataContext';
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 const Home = () => {
 
-  const {searchResults,isloading,fetchError} = useContext(DataContext);
+  const { searchResults, isloading, fetchError } = useContext(DataContext);
   return (
     <main className='Home'>
-    {isloading && <p className='statusMsg' >Loading posts ....</p>}
-    {!isloading && fetchError && <p className='statusMsg' style={{color:"red"}} >{fetchError}</p>}
-    {!isloading && !fetchError &&(searchResults.length ? <Feed posts={searchResults} /> : "No posts found.")}
+      {isloading && (
+        <>
+          <Skeleton height={30} width={200} />
+          <Skeleton count={2} />
+          <Skeleton height={30} width={200} />
+          <Skeleton count={2} />
+          <Skeleton height={30} width={200} />
+          <Skeleton count={2} />
+          <Skeleton height={30} width={200} />
+          <Skeleton count={2} />
+          <Skeleton height={30} width={200} />
+          <Skeleton count={2} />
+          <Skeleton height={30} width={200} />
+          <Skeleton count={2} />
+        </>
+      )}
+      {!isloading && fetchError && <p className='statusMsg' style={{ color: "red" }} >{fetchError}</p>}
+      {!isloading && !fetchError && (
+        searchResults.length ? (
+          <Feed posts={searchResults} />
+        ) : (
+          <>
+            <Skeleton height={30} width={200} />
+            <Skeleton count={2} />
+            <Skeleton height={30} width={200} />
+            <Skeleton count={2} />
+            <Skeleton height={30} width={200} />
+            <Skeleton count={2} />
+            <Skeleton height={30} width={200} />
+            <Skeleton count={2} />
+            <Skeleton height={30} width={200} />
+            <Skeleton count={2} />
+            <Skeleton height={30} width={200} />
+            <Skeleton count={2} />
+
+          </>
+        )
+      )}
+
     </ main>
   )
 }
